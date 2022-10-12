bf064882ec418083d03312c38dc693ef715da73c
Slava <tfdf63@yandex.ru>
2022-10-12 12:07:26 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+const questions = [
+	{
+		question: "Какой язык работает в браузере?",
+		answers: ["Java", "C", "Python", "JavaScript"],
+		correct: 4,
+	},
+	{
+		question: "Что означает CSS?",
+		answers: [
+			"Central Style Sheets",
+			"Cascading Style Sheets",
+			"Cascading Simple Sheets",
+			"Cars SUVs Sailboats",
+		],
+		correct: 2,
+	},
+	{
+		question: "Что означает HTML?",
+		answers: [
+			"Hypertext Markup Language",
+			"Hypertext Markdown Language",
+			"Hyperloop Machine Language",
+			"Helicopters Terminals Motorboats Lamborginis",
+		],
+		correct: 1,
+	},
+	{
+		question: "В каком году был создан JavaScript?",
+		answers: ["1996", "1995", "1994", "все ответы неверные"],
+		correct: 2,
+	},
+];
+
+//Находим элементы
+const headerContainer = document.querySelector('#header');
+const listContainer = document.querySelector('#list');
+const submitBtn = document.querySelector('#submit');
+
+//Переменные игры
+let score = 0;
+let questionIndex = 0;
+
+clearPage();
+showQuestion();
+
+function clearPage() {
+	headerContainer.innerHTML = '';
+	listContainer.innerHTML = '';
+}
+
+function showQuestion() {
+	//Вопрос
+	//Создали метку %title% и с помощью replece заменили её значение на значение из массива
+	const headerTemplate = `<h2 class="title">%title%</h2>`;
+	const title = headerTemplate.replace('%title%', questions[questionIndex]['question']);
+
+	//Поместили значение переменной title в разметку
+	headerContainer.innerHTML = title;
+
+	//Варианты ответов
+	for (item of questions[questionIndex]['answers']) {
+		console.log(item);
+
+
+		//ПРОДОЛЖАЕМ ТУТ
+	}
+}
