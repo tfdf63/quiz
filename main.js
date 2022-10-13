39f487319b9a9b9ceacdeb2b4a587992acdcffeb
Slava <tfdf63@yandex.ru>
2022-10-13 19:50:19 +0400

    quiz itog 13.10

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 const questions = [
 	{
-		question: "Какой язык работает в браузере?",
-		answers: ["Java", "C", "Python", "JavaScript"],
-		correct: 4,
+		question: "Сколько будет 2+2*2",
+		answers: ["8", "6", "4", "2"],
+		correct: 2,
 	},
 	{
-		question: "Что означает CSS?",
+		question: "Моя должность в футбольном клубе Акрон?",
 		answers: [
-			"Central Style Sheets",
-			"Cascading Style Sheets",
-			"Cascading Simple Sheets",
-			"Cars SUVs Sailboats",
+			"Главный по тарелочкам",
+			"Специалист по работе с болельщиками",
+			"SMM",
+			"Тренер по силовой подготовке",
 		],
 		correct: 2,
 	},
 	{
-		question: "Что означает HTML?",
+		question: "С кем играет ФК Акрон 23 октября на Кристалле в Жигулевске?",
 		answers: [
-			"Hypertext Markup Language",
-			"Hypertext Markdown Language",
-			"Hyperloop Machine Language",
-			"Helicopters Terminals Motorboats Lamborginis",
+			"ФК Арсенал",
+			"ФК Факел",
+			"ФК Торпедо",
+			"ФК Рубин",
 		],
-		correct: 1,
+		correct: 4,
 	},
 	{
-		question: "В каком году был создан JavaScript?",
-		answers: ["1996", "1995", "1994", "все ответы неверные"],
+		question: "В каком году я начал работать в ФК Акрон?",
+		answers: ["2022", "2021", "2012", "2018"],
 		correct: 2,
 	},
 ];
@@ -61,35 +61,90 @@ function showQuestion() {
 	//Варианты ответов
 	let answerNumber = 1;
 	for ([index, answersText] of questions[questionIndex]['answers'].entries()) {
-		console.log(index+1, answersText);
 
-	const questionTemplate =
-		`<li>
+		const questionTemplate =
+			`<li>
 			<label>
 				<input value="%number%" type="radio" class="answer" name="answer" />
 				<span>%answer%</span>
 			</label>
 		</li>`;
-	
-	const answerHTML = questionTemplate
-							.replace('%answer%', answersText)
-							.replace('%number%', answerNumber);
 
-	listContainer.innerHTML += answerHTML;
-	answerNumber++;
+		const answerHTML = questionTemplate
+			.replace('%answer%', answersText)
+			.replace('%number%', answerNumber);
+
+		listContainer.innerHTML += answerHTML;
+		answerNumber++;
 	}
 };
 
-function checkAnswer () {
+function checkAnswer() {
 	//Находим выбранную радио кнопку
 	const checkedRadio = listContainer.querySelector('input[type="radio"]:checked');
 
 	//Если ответ не выбран - ничего не делаем, выходим из функции
-	if (!checkedRadio){
+	if (!checkedRadio) {
 		submitBtn.blur();
-		return
-	};
+		return;
+	}
 
 	//Узнаем номер ответа пользователя
 	const userAnswer = parseInt(checkedRadio.value);
+
+	//Если ответ верно - увеличиваем счёт
+	console.log(userAnswer, questions[questionIndex]['correct']);
+	if (userAnswer === questions[questionIndex]['correct']) {
+		score++;
+	}
+
+	if (questionIndex !== questions.length - 1) {
+		console.log('Это НЕ последний вопрос');
+		questionIndex++;
+		clearPage();
+		showQuestion();
+	} else {
+		console.log('Это последний вопрос');
+		clearPage();
+		showResults();
+	}
+}
+
+function showResults() {
+	console.log('showResults start');
+	console.log(score);
+
+	const resultsTemplate = `
+			<h2 class="title">%title%</h2>
+			<h3 class="summary">%message%</h3>
+			<p class="result">%result%</p>
+		`;
+
+	let title, message;
+	//Варианты заголовков ответов и текста
+	if (score === questions.length) {
+		title = 'Поздравляем';
+		message = 'Вы ответили верно на все вопросы';
+	} else if ((score * 100) / questions.length >= 50) {
+		title = 'Неплохой результат!';
+		message = 'Вы дали более половины правильных ответов';
+	} else {
+		title = 'Стоит постараться';
+		message = 'Пока у вас меньше половины правильных ответов';
+	}
+
+	let result = `${score} из ${questions.length}`;
+
+	//Финальный ответ
+	const finalMessage = resultsTemplate
+		.replace('%title%', title)
+		.replace('%message%', message)
+		.replace('%result%', result);
+
+	headerContainer.innerHTML = finalMessage;
+
+	//Меняем кнопку на играть снова
+	submitBtn.blur();
+	submitBtn.innerText = 'Начать заново';
+	submitBtn.onclick = () => history.go();
 }
